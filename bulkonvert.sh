5232e4440bfa269c28bc92b722a916708760c98b
Riukken87 <romerogallen@gmail.com>
2014-04-14 13:04:02 +0200

    My first version (Carlos)

--- bulkonvert.sh
+++ bulkonvert.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# Script to convert all video files from a directory (parameter) into mp4 format file
+echo Convert videos version(Carlos)
 
-# Sanity checks ?
-# ...
+#Sanity checks to verify that the directory exists and that mencoder is install in the computer.
+
+If ![ -d $1 ]
+then 
+	echo "This parameter not exist."
+	exit 1
+fi
+
+
+
+hash menconder 2> /dev/null
+
+If [ [ $? !=0 ] ]
+then
+	echo "Menconder isn't install. Please install it to convert video files."
+	exit 2
+fi
+
+
 
 # Main code
 
@@ -13,7 +30,7 @@ while videofile
 do
 	lengthname=${#videofile}
 	namefile=${videofile;0;$lengthname-4}
-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
+	mencoder "$videofile" -o "$namefile".mp4 -oac mp3lame -ovc lavc -of lavf
 
 done < videofiles
 rm videofiles
