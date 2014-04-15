fb314708b66f6b324b493634d1da7003bfb17b2a
Riukken87 <romerogallen@gmail.com>
2014-04-15 10:26:54 +0200

    second version

--- bulkonvert.sh
+++ bulkonvert.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo Convert videos version(Carlos)
+
+echo Convert videos version'(Carlos)'
+echo We can convert avi, mpeg, mp4, mp3
 
 #Sanity checks to verify that the directory exists and that mencoder is install in the computer.
 
-If ![ -d $1 ]
+if [ ! -d $1 ];
 then 
-	echo "This parameter not exist."
+	echo "This parameter not exist: $1"
 	exit 1
 fi
 
 
 
-hash menconder 2> /dev/null
+hash mencoder 2> /dev/null
 
-If [ [ $? !=0 ] ]
+if [ $? != 0 ];
 then
 	echo "Menconder isn't install. Please install it to convert video files."
 	exit 2
@@ -26,10 +28,10 @@ fi
 
 ls -1 $1*.avi > videofiles
 
-while videofile
+while read videofile
 do
 	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
+	namefile=${videofile:0:$lengthname-4}
 	mencoder "$videofile" -o "$namefile".mp4 -oac mp3lame -ovc lavc -of lavf
 
 done < videofiles
